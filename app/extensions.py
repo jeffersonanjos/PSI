903ac37064a9015c9aa50c033d37621ba5e0fb77
jefferson <jefferson.anjos@escolar.ifrn.edu.br>
2025-10-14 21:43:48 -0300

    imagem do sistema

--- app/extensions.py
+++ app/extensions.py
@@ -3,9 +3,6 @@ from flask_bcrypt import Bcrypt
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
-# Mensagem de login em português e categoria de alerta amigável
-login_manager.login_message = 'Por favor, faça login para acessar esta página.'
-login_manager.login_message_category = 'warning'
 
 # Password hashing
 bcrypt = Bcrypt()
